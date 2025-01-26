f5d3b74236f5599775129a45f9d6f2493622e382
Guillaume Rossignol <rossignolgui@free.fr>
2025-01-26 20:26:36 +0100

    gestion du mono

--- aide-dodo.sh
+++ aide-dodo.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-getCurrentVolume () { amixer get Master|sed -nE 's/Front.*Left.*\[([0-9]*)%\].*/\1/p';}
+getCurrentVolume () { amixer get Master|sed -nE 's/(Front.*Left.*|Mono.*)\[([0-9]*)%\].*/\2/p';}
 currentVolume="$(getCurrentVolume)"
 
 attenteInitiale=600 #On attend dix minutes avant de commencer à baisser le son
 dureeDeBaisse=1500  #On baisse le volume pendant 25 minutes
 nombreDeBaisse=25
 
-attenteInitiale=1
-dureeDeBaisse=60
-nombreDeBaisse=10
+#attenteInitiale=1
+#dureeDeBaisse=60
+#nombreDeBaisse=10
 
 frequenceDeBaisse="$(( $dureeDeBaisse / $nombreDeBaisse ))"
 valeurDeBaisse="$(( $currentVolume / $nombreDeBaisse ))"
 
-
+echo "$currentVolume";exit 0;
 sleep $attenteInitiale
 while [ $currentVolume -gt 0 ];
 do
